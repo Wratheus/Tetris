b5abd6e9ff754a77646ef943a038c0ec3a63a80e
Aleksandr Pavlenko <a.pavlenko@lichi.me>
2025-08-28 18:29:56 +0300

    add movement hold mode

--- lib/feature/game/widgets/controllers/keyboard_controller.dart
+++ lib/feature/game/widgets/controllers/keyboard_controller.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
 import 'package:tetris/feature/game/game_notifier.dart';
 
-///keyboard controller to play game
+///keyboard controller to play game with key hold support
 class KeyboardGameController extends StatefulWidget {
   const KeyboardGameController({
     required this.gameController,
@@ -20,11 +21,75 @@ class KeyboardGameController extends StatefulWidget {
 
 class _KeyboardGameControllerState extends State<KeyboardGameController> {
   final FocusNode _focusNode = FocusNode();
+  
+  // Таймеры для зажатия клавиш
+  Timer? _leftTimer;
+  Timer? _rightTimer;
+  Timer? _downTimer;
+  
+  // Флаги для отслеживания зажатых клавиш
+  bool _isLeftPressed = false;
+  bool _isRightPressed = false;
+  bool _isDownPressed = false;
+  
+  // Задержка перед началом повторения
+  static const Duration _initialDelay = Duration(milliseconds: 200);
+  // Интервал повторения
+  static const Duration _repeatInterval = Duration(milliseconds: 100);
 
   @override
   void dispose() {
+    _cancelAllTimers();
     _focusNode.dispose();
     super.dispose();
+  }
+
+  void _cancelAllTimers() {
+    _leftTimer?.cancel();
+    _rightTimer?.cancel();
+    _downTimer?.cancel();
+    _leftTimer = null;
+    _rightTimer = null;
+    _downTimer = null;
+  }
+
+  void _startLeftRepeat() {
+    if (_isLeftPressed && _leftTimer == null) {
+      _leftTimer = Timer.periodic(_repeatInterval, (timer) {
+        if (_isLeftPressed) {
+          widget.gameController.left();
+        } else {
+          timer.cancel();
+          _leftTimer = null;
+        }
+      });
+    }
+  }
+
+  void _startRightRepeat() {
+    if (_isRightPressed && _rightTimer == null) {
+      _rightTimer = Timer.periodic(_repeatInterval, (timer) {
+        if (_isRightPressed) {
+          widget.gameController.right();
+        } else {
+          timer.cancel();
+          _rightTimer = null;
+        }
+      });
+    }
+  }
+
+  void _startDownRepeat() {
+    if (_isDownPressed && _downTimer == null) {
+      _downTimer = Timer.periodic(_repeatInterval, (timer) {
+        if (_isDownPressed) {
+          widget.gameController.down();
+        } else {
+          timer.cancel();
+          _downTimer = null;
+        }
+      });
+    }
   }
 
   bool _onKey(BuildContext context, KeyEvent event) {
@@ -35,26 +100,82 @@ class _KeyboardGameControllerState extends State<KeyboardGameController> {
           key == LogicalKeyboardKey.digit2 ||
           key == LogicalKeyboardKey.numpad2) {
         widget.gameController.rotate();
+        return true;
       } else if (key == LogicalKeyboardKey.arrowDown ||
           key == LogicalKeyboardKey.digit8 ||
           key == LogicalKeyboardKey.numpad8) {
-        widget.gameController.down();
+        if (!_isDownPressed) {
+          _isDownPressed = true;
+          widget.gameController.down();
+          // Запускаем повторение с задержкой
+          Timer(_initialDelay, () {
+            if (_isDownPressed) {
+              _startDownRepeat();
+            }
+          });
+        }
+        return true;
       } else if (key == LogicalKeyboardKey.arrowLeft ||
           key == LogicalKeyboardKey.digit4 ||
           key == LogicalKeyboardKey.numpad4) {
-        widget.gameController.left();
+        if (!_isLeftPressed) {
+          _isLeftPressed = true;
+          widget.gameController.left();
+          // Запускаем повторение с задержкой
+          Timer(_initialDelay, () {
+            if (_isLeftPressed) {
+              _startLeftRepeat();
+            }
+          });
+        }
+        return true;
       } else if (key == LogicalKeyboardKey.arrowRight ||
           key == LogicalKeyboardKey.digit6 ||
           key == LogicalKeyboardKey.numpad6) {
-        widget.gameController.right();
+        if (!_isRightPressed) {
+          _isRightPressed = true;
+          widget.gameController.right();
+          // Запускаем повторение с задержкой
+          Timer(_initialDelay, () {
+            if (_isRightPressed) {
+              _startRightRepeat();
+            }
+          });
+        }
+        return true;
       } else if (key == LogicalKeyboardKey.space ||
           key == LogicalKeyboardKey.enter ||
           key == LogicalKeyboardKey.numpadEnter) {
         widget.gameController.drop();
-      } else {
-        return false;
+        return true;
       }
-      return true;
+      return false;
+    } else if (event is KeyUpEvent) {
+      final key = event.logicalKey;
+      
+      // Обрабатываем отпускание клавиш
+      if (key == LogicalKeyboardKey.arrowDown ||
+          key == LogicalKeyboardKey.digit8 ||
+          key == LogicalKeyboardKey.numpad8) {
+        _isDownPressed = false;
+        _downTimer?.cancel();
+        _downTimer = null;
+        return true;
+      } else if (key == LogicalKeyboardKey.arrowLeft ||
+          key == LogicalKeyboardKey.digit4 ||
+          key == LogicalKeyboardKey.numpad4) {
+        _isLeftPressed = false;
+        _leftTimer?.cancel();
+        _leftTimer = null;
+        return true;
+      } else if (key == LogicalKeyboardKey.arrowRight ||
+          key == LogicalKeyboardKey.digit6 ||
+          key == LogicalKeyboardKey.numpad6) {
+        _isRightPressed = false;
+        _rightTimer?.cancel();
+        _rightTimer = null;
+        return true;
+      }
     }
     return false;
   }
